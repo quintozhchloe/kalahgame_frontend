cd54380964e44198ce2d14ea5f1657b4903d9c66
chloeH <167677653+quintozhchloe@users.noreply.github.com>
2024-10-02 22:44:11 +1300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import App from './App';
-import theme from './theme'; // 导入主题
+import theme from './theme'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
